7749c03fa3ef8024e76ff6084568c6b27896e50f
Anas Shaikh <shaikhanas246@gmail.com>
2024-11-21 22:30:57 +0530

    embedding practice

--- node-mongoose/project-1/index.js
+++ node-mongoose/project-1/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -14,28 +16,34 @@ async function main() {
 
 main().catch((err) => console.log(err));
 
+
+
+
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  courses: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'courses'
+  }]
 });
 
 const User = mongoose.model('User', UserSchema);
 
-app.get('/create', async (req, res) => {
+app.get('/user/create', async (req, res) => {
 
   const { name, email, password } = req.query
   const user = await User.insertMany({ name: name, email: email, password: password })
 
   res.status(200).json(user)
 })
-app.get('/find', async (req, res) => {
+app.get('/user/find', async (req, res) => {
 
   const user = await User.find({});
   res.status(200).json(user)
 
 })
-app.get('/update', async (req, res) => {
+app.get('/user/update', async (req, res) => {
 
   const { id, name, email, password } = req.query;
 
@@ -47,11 +55,39 @@ app.get('/update', async (req, res) => {
   })
 
 })
-app.get('/delete', async (req, res) => {
+app.get('/user/delete', async (req, res) => {
 
   const { id } = req.query;
 
   const user = await User.deleteOne({ _id: id })
   res.status(200).json(user)
+
+})
+
+
+
+const CourseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  users: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'users'
+  }]
+})
+
+const Course = mongoose.model('course', CourseSchema);
+
+
+app.get('/course/create', async (req, res) => {
+
+  const { title, description, userId } = req.query
+
+  const course = await Course.create({ title: title, description: description, users: [userId] });
+  // const userCourses = await User.findOne({ _id: userId })
+
+  // console.log(userCourses.courses)
+
+  await User.updateOne({ _id: userId }, { $set: { courses: [userCourses, course._id] } })
+
+  // res.status(200).json(course)
 
 })
