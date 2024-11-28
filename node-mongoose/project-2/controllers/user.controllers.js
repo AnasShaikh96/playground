1917c5d8e8959cf8983c444bf0f7c69bb8de5a17
Anas Shaikh <shaikhanas246@gmail.com>
2024-11-28 22:09:47 +0530

    wip: add Api res standard

--- node-mongoose/project-2/controllers/user.controllers.js
+++ node-mongoose/project-2/controllers/user.controllers.js
@@ -1,32 +1,55 @@
 import { User } from "../models/user.models.js"
 import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(
+  async (req, res) => {
+    // check if either thing is missing
+    const { userName, fullName, email, password } = req.body
 
-  const { userName, fullName, email, password } = req.body
-
-  try {
-    const user = await User.create({ userName, fullName, email, password })
-
-    res.status(200).json(user)
-
-  } catch (error) {
-
-    // res.status(500)
-
-    throw new ApiError(500, error.message)
-  }
+    if ([userName, fullName, email, password].some(content => content.trim().length === 0)) {
+      throw new ApiError(400, 'Empty values are not allowed')
+    }
 
 
-}
+    const existedUser = await User.findOne({
+      $or: [{ userName }, { email }]
+    })
+
+    if (existedUser) {
+      throw new ApiError(400, 'UserName or Email already Exists!')
+    }
+
+    try {
+      const user = await User.create({ userName, fullName, email, password })
+
+      res.status(200).json(
+        new ApiResponse(200, user, "User created Successfully")
+      );
+
+    } catch (error) {
+
+      // console.log(error)
+
+      throw new ApiError(500, error.message)
+
+      // console.log('hello', error)
+
+      // res.status(error.status).json(
+      //   new ApiError(error.status, error.message)
+      // )
+
+    }
+  })
 
 
-const getUsers = async (req, res) => {
+const getUsers = asyncHandler(async (req, res) => {
 
   const users = await User.find();
 
   res.status(200).json(users)
-}
+})
 
 
 export { createUser, getUsers }
