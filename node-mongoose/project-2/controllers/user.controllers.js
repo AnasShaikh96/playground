5d0cd2021a23437e55bdedf8c7c8e79d7e22bde7
Anas Shaikh <shaikhanas246@gmail.com>
2024-11-30 20:35:47 +0530

    access and refresh tokens

--- node-mongoose/project-2/controllers/user.controllers.js
+++ node-mongoose/project-2/controllers/user.controllers.js
@@ -3,6 +3,22 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
+
+const generateAccessAndRefreshToken = async (userId) => {
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, 'User does not exists!')
+  }
+
+  const accessToken = user.generateAccessToken()
+  const refreshToken = user.generateRefreshToken()
+
+  return { accessToken, refreshToken }
+
+}
+
 const createUser = asyncHandler(
   async (req, res) => {
     // check if either thing is missing
@@ -32,36 +48,50 @@ const createUser = asyncHandler(
   })
 
 
-const loginUser = asyncHandler(async (res, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   // validate is in correct format email
   // if email exists
   // if password is correct
   // generate tokens and return vals
 
-  const { email, password } = req.body;
+  const { email, userName, password } = req.body;
 
-  if ([email, password].some(field => field.trim() === '')) {
-    throw new ApiError(400, 'Email or password cannot be empty')
-  }
+  // if ([email, userName, password].some(field => field.trim() === '')) {
+  //   throw new ApiError(400, 'Email or password cannot be empty')
+  // }
 
 
-  const findEmail = await User.findOne({
-    $or: [{ email }]
+  const user = await User.findOne({
+    $or: [{ email }, { userName }]
   })
 
-  if (!findEmail) {
+  if (!user) {
     throw new ApiError(400, 'User does not Exists');
   }
 
-  const checkPassword = findEmail.isPasswordCorrect(password)
+  const checkPassword = user.isPasswordCorrect(password)
 
   if (!checkPassword) {
     throw new ApiError(400, 'password is incorrect');
   }
 
 
-})
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
 
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
+  return res.status(200)
+    .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, options)
+    .json(
+      new ApiResponse(200, user, "User Logged in Successfully!")
+    )
+
+
+})
 
 
 const getUsers = asyncHandler(async (req, res) => {
--- node-mongoose/project-2/routes/user.routes.js
+++ node-mongoose/project-2/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { createUser, getUsers } from '../controllers/user.controllers.js';
+import { createUser, getUsers, loginUser } from '../controllers/user.controllers.js';
 
 const router = Router()
 
 router.route('/create').post(createUser);
 router.route('/find').get(getUsers);
+router.route('/login').post(loginUser);
 
 export default router
